52b7bc81d9ed807ae93acc3a7cc175a5b19a983e
PedroBazilio <33727038+PedroBazilio@users.noreply.github.com>
2021-06-05 23:32:36 -0300

    Commit inicial

---
+++ toDo.js
@@ -0,0 +1,31 @@
+/*aplicação que consiste em listas e tarefas.
+Uma lista terá um identificador, um nome e um array de tarefas
+Uma tarefa consiste em um identificador, uma descrição e um atributo para tarefa feita ou não
+
+Funcionalidades Implementadas
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+let lista = {
+
+    "id": "lista-0",
+    "nome": "Lista",
+    "tarefas": [],
+    "numTarefas": 0
+}
+
+let tarefa = {
+    "id": "tarefa-0",
+    "descricao": "Comprar leite",
+    "completa": false
+}
